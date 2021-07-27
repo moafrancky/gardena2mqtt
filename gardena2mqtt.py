b33c3286bcdf2b491c8c51df62976f45d24c96d5
FB <8932050+moafrancky@users.noreply.github.com>
2021-07-27 20:39:23 +0200

    set retain=true for device payloads

--- gardena2mqtt.py
+++ gardena2mqtt.py
@@ -9,12 +9,12 @@ import paho.mqtt.client as mqtt
 
 def publish_device(device):
     deviceLocation = device.location.name
-    mqttclient.publish(f"{mqttprefix}/{deviceLocation}/{device.name}/datetime", time.strftime("%Y-%m-%d %H:%M:%S"))
+    mqttclient.publish(f"{mqttprefix}/{deviceLocation}/{device.name}/datetime", time.strftime("%Y-%m-%d %H:%M:%S"), 0, True)
 
     for attrName in vars(device):
         if not attrName.startswith('_') and attrName not in ('location', 'callbacks'):
             attrValue = getattr(device, attrName)
-            mqttclient.publish(f"{mqttprefix}/{deviceLocation}/{device.name}/{attrName}", attrValue)
+            mqttclient.publish(f"{mqttprefix}/{deviceLocation}/{device.name}/{attrName}", attrValue, 0, True)
 
 def publish_everything():
     global smart_system
